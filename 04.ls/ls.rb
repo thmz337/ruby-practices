aac1d46563aa99b57f7e4bfbb60ce7d682f84232
thmz337 <th994209@gmail.com>
2024-05-23 22:49:04 +0900

    rubocop等をグローバルにインストールしたため、Gemfileを削除 変数名の変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,24 +4,22 @@
 
 MAX_COLUMN = 3
 
-def current_directory_files
+def current_directory_file_names
   Dir.glob('*')
 end
 
-def make_matrix(files)
-  number_of_files = files.size
+def make_matrix(file_names)
+  num_of_file_names = file_names.size
 
-  col = (number_of_files % MAX_COLUMN).zero? ? number_of_files.div(MAX_COLUMN) : number_of_files.div(MAX_COLUMN) + 1
+  num_of_cols = (num_of_file_names % MAX_COLUMN).zero? ? num_of_file_names.div(MAX_COLUMN) : num_of_file_names.div(MAX_COLUMN) + 1
 
-  files.each_slice(col).map do |fs|
-    fs.fill('', fs.size..(col - 1)) unless fs.size == col
-    longest_file_name = fs.max_by(&:length).length
+  file_names.each_slice(num_of_cols).map do |matrix_row|
+    matrix_row.fill('', matrix_row.size..(num_of_cols - 1)) unless matrix_row.size == num_of_cols
+    longest_file_name = matrix_row.max_by(&:length).length
 
-    fs.map do |f|
-      fd = f.dup
-      diff = longest_file_name - f.length
-      fd.concat(' ' * diff) if diff.positive?
-      fd.concat(' ' * 4)
+    matrix_row.map do |file_name|
+      diff = longest_file_name - file_name.length
+      "#{file_name}#{' ' * diff if diff.positive?}#{' ' * 4}"
     end
   end
 end
@@ -30,10 +28,10 @@ def make_display_text(matrix)
   matrix.map { |m| m.join.rstrip!.concat("\n") }.join
 end
 
-files = current_directory_files
+file_names = current_directory_file_names
 
-unless files.empty?
-  matrix = make_matrix(files)
+unless file_names.empty?
+  matrix = make_matrix(file_names)
   display_matrix = matrix.transpose
   display_text = make_display_text(display_matrix)
   puts display_text
